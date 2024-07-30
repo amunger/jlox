ac430ceed938274712ee754a49fc12a753f1620f
aamunger <aamunger@microsoft.com>
2024-07-30 10:30:42 -0700

    chapter 6 parser

---
+++ com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,145 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import static  com.craftinginterpreters.lox.TokenType.*;
+
+class Parser {
+    private static class ParseError extends RuntimeException {}
+
+    private final List<Token> tokens;
+    private int current = 0;
+    
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    Expr parse() {
+        try {
+            return expression();
+        } catch (ParseError error) {
+            return null;
+        }
+    }
+
+    private Expr expression() {
+        return equality();
+    }
+
+    private Expr equality() {
+        return parseExpr(this::comparison, EQUAL_EQUAL, BANG_EQUAL);
+    }
+
+    private Expr comparison() {
+        return parseExpr(this::term, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL);
+    }
+
+    private Expr term() {
+        return parseExpr(this::factor, MINUS, PLUS);
+    }
+
+    private Expr factor() {
+        return parseExpr(this::unary, SLASH, STAR);
+    }
+
+    private Expr parseExpr(Supplier<Expr> nextPrecendent, TokenType... types) {
+        Expr expr = nextPrecendent.get();
+
+        while (match(types)) {
+            Token operator = previous();
+            Expr right = nextPrecendent.get();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr unary() {
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    private Expr primary() {
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+
+        if (match(NUMBER, STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+
+        throw error(peek(), "Expected Expression");
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) return advance();
+
+        throw error(peek(), message);
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
+    }
+
+    private void synchronize() {
+        advance();
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON) return;
+
+            switch (peek().type) {
+                case CLASS: case FOR: case FUN: case IF: case PRINT: case RETURN: case VAR: case WHILE:
+                    return;
+                default:
+            }
+
+            advance();
+        }
+    }
+
+}
--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -68,10 +68,10 @@ public class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
-            case '!': addToken(match('=') ? BANG_EQUAL : BANG);
-            case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL);
-            case '<': addToken(match('=') ? LESS_EQUAL : LESS);
-            case '>': addToken(match('=') ? GREATER_EQUAL : GREATER);
+            case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
+            case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
+            case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
+            case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
             case '/':
                 if (match('/')) {
                     while (peek() != '\n' && !isAtEnd()) advance();
