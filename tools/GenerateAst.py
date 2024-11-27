0e93419002b3afe7fdb99b3ab7bae4f39fc3361a
amunger <aamunger@microsoft.com>
2024-11-27 12:42:43 -0800

    chapter 10 and 11, functions and the resolver

--- tools/GenerateAst.py
+++ tools/GenerateAst.py
@@ -62,6 +62,7 @@ def defineType(file, baseName, className, fieldList):
 defineAst("Expr", [
     "Assign   : Token name, Expr value",
     "Binary   : Expr left, Token operator, Expr right",
+    "Call     : Expr callee, Token paren, List<Expr> arguments",
     "Grouping : Expr expression",
     "Literal  : Object value",
     "Logical  : Expr left, Token operator, Expr right",
@@ -72,8 +73,10 @@ defineAst("Expr", [
 defineAst("Stmt", [
     "Block      : List<Stmt> statements",
     "Expression : Expr expression",
+    "Function   : Token name, List<Token> params, List<Stmt> body",
     "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
-    "Var        : Token name, Expr initializer",
     "Print      : Expr expression",
+    "Return     : Token keyword, Expr value",
+    "Var        : Token name, Expr initializer",
     "While      : Expr condition, Stmt body",
 ])
